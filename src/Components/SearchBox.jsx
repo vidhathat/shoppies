5f79e10f64191e8f77a6862a4dfa6b1342a397d4
vidhatha <vtvidhathatallapally@gmail.com>
2021-01-16 18:43:08 +0530

    secured server

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -7,7 +7,7 @@ function SearchBox(props) {
     let [searchInput, setSearchInput] = useState('');
     const handleInputChange = () => {
         if(searchInput.value) {
-            fetch(`http://www.omdbapi.com/?s=${searchInput.value}&apikey=cd35c9de`)
+            fetch(`https://www.omdbapi.com/?s=${searchInput.value}&apikey=cd35c9de`)
         .then(res => res.json())
         .then((data) => {
             if(data.Response === 'False' || data.Response === 'True') {
